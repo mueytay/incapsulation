96d3a7b1a63aaa05facc1dd68638770c6581874e
MSI <mueytay38@gmail.com>
2026-06-10 17:21:26 +0300

    Вроде разобрался , в начале с пакетами напутал

---
+++ src/org/skypro/skyshop/App.java
@@ -0,0 +1,30 @@
+package org.skypro.skyshop;
+
+import org.skypro.skyshop.basket.ProductBasket;
+import org.skypro.skyshop.product.Product;
+
+public class App {
+    public static void main(String[] args) {
+        ProductBasket productbasket = new ProductBasket();
+
+        Product product1 = new Product("яблоко", 50);
+        Product product2 = new Product("персик", 80);
+        Product product3 = new Product("помидор", 90);
+        Product product4 = new Product("авокадо", 95);
+        Product product5 = new Product("лук", 20);
+       // Product product6 = new Product("jjj", 77);
+        productbasket.addProduct(product1);
+        productbasket.addProduct(product2);
+        productbasket.addProduct(product3);
+        productbasket.addProduct(product4);
+        productbasket.addProduct(product5);
+       // productbasket.addProduct(product6);
+
+        productbasket.printBasket();
+        System.out.println("есть ли молоко"+ productbasket.presentsProduct("молоко"));
+        productbasket.clean();
+        productbasket.printBasket();
+    }
+
+}
+
---
+++ src/org/skypro/skyshop/basket/ProductBasket.java
@@ -0,0 +1,66 @@
+package org.skypro.skyshop.basket;
+
+import org.skypro.skyshop.product.Product;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ProductBasket {
+    private Product[] products;
+    private int count;
+
+    public ProductBasket() {
+        this.products = new Product[5];
+        this.count = 0;
+    }
+
+
+    public boolean addProduct(Product product) {
+        if (count >= 5) {
+            System.out.println("Корзина полна");
+            return false;
+        }
+
+        products[count] = product;
+        count++;
+        return true;
+    }
+
+    public int getTotalCost() {
+        int total = 0;
+        for (int i = 0; i < count; i++) {
+            if (products[i] != null) {
+                total += products[i].getPrice();
+            }
+        }
+        return total;
+    }
+
+    public void printBasket() {
+        if (count == 0) {
+            System.out.println("Корзина пуста");
+            return;
+        }
+
+        for (int i = 0; i < count; i++) {
+            System.out.println(products[i].getName() + ":" + products[i].getPrice());
+        }
+        System.out.println("Итого" + getTotalCost());
+    }
+
+    public boolean presentsProduct(String name) {
+        for (int i = 0; i < count; i++) {
+            if (products[i] != null && products[i].getName().equals(name)) {
+                return true;
+            }
+        }
+        return false;
+    }
+    public void clean(){
+        for (int i=0;i<products.length;i++){
+            products[i]=null;
+        }
+        count=0;
+    }
+}
+
